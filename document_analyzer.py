8ec445f3b0f6ed0a0397539ead04a02747ab3579
AnhNguyen618 <99697353+AnhNguyen618@users.noreply.github.com>
2022-03-10 16:36:19 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,23 +1,24 @@
 import string
 
+def document_analyzer():
+    with open("document.txt", 'r', encoding="utf-8") as f:
+        for line in f:
+            line = line.translate(line.maketrans("","",string.punctuation))
+            tokens = line.split()
+        
 
-with open("document.txt", 'r', encoding="utf-8") as f:
-    for line in f:
-        line = line.translate(line.maketrans("","",string.punctuation))
-        tokens = line.split()
-    
+    dict1 = {}
+    for t in tokens:
+        if t in dict1:
+            dict1[t] = dict1[t] + 1
+        else:
+            dict1[t] = 1
 
-dict1 = {}
-for t in tokens:
-    if t in dict1:
-        dict1[t] = dict1[t] + 1
-    else:
-        dict1[t] = 1
+    sorted_dict = sorted(dict1.items(), key=lambda x: (-x[1], x[0]))
+    final_sorted_dict =list(sorted_dict)[:5]
 
-sorted_dict = sorted(dict1.items(), key=lambda x: (-x[1], x[0]))
-final_sorted_dict =list(sorted_dict)[:5]
+    print()
+    for j in final_sorted_dict :
+        print(j[0], ": ", j[1])
 
-print()
-for j in final_sorted_dict :
-    print(j[0], ":", j[1])
-   
+document_analyzer()
